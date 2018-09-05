aa9ed1ceb03f7579a063199d508fba5d2911f44a
MichalPC1 <banka.michal1964@gmail.com>
2018-09-05 21:18:32 +0200

    Added JWT Authetication Filter

---
+++ src/main/java/com/app/security/JWTAutheticationFilter.java
@@ -0,0 +1,62 @@
+package com.app.security;
+
+        import com.app.models.User;
+        import com.fasterxml.jackson.databind.ObjectMapper;
+        import io.jsonwebtoken.Jwts;
+        import io.jsonwebtoken.SignatureAlgorithm;
+        import org.springframework.security.authentication.AuthenticationManager;
+        import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+        import org.springframework.security.core.Authentication;
+        import org.springframework.security.core.AuthenticationException;
+        import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+        import javax.servlet.FilterChain;
+        import javax.servlet.ServletException;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.Date;
+
+public class JWTAutheticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAutheticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUsername(),
+                            user.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        //now generate token for succesfully logged user
+        String token = Jwts.builder()
+                //data part
+                .setSubject((/*cast to this ->*/(org.springframework.security.core.userdetails.User) authResult.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                //sign part
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+        //header part
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
